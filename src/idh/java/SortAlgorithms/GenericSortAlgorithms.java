841b2f80efff811b608038d92375b5bf89dba185
kaste <kaste@LAPTOP-S3KPO7GD.fritz.box>
2025-07-01 11:31:38 +0200

    exercise 8

--- src/idh/java/SortAlgorithms/GenericSortAlgorithms.java
+++ src/idh/java/SortAlgorithms/GenericSortAlgorithms.java
@@ -8,7 +8,16 @@ package idh.java.SortAlgorithms;
 public class GenericSortAlgorithms <T extends Comparable<T>>{
 	
 	public void bubbleSort(T[] arr) {
-        
+		int n = arr.length;
+        for (int i = 0; i < n-1; i++) {
+            for (int j = 0; j < n-i-1; j++) {
+                if (arr[j].compareTo(arr[j+1]) > 0) {
+                    // Swap arr[j+1] and arr[j]
+                    T temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp; }
+            }
+        }
     }
 
     public void selectionSort(T[] arr) {
